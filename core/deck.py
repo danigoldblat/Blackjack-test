613dabf6768991c1f132bd7dcf2d763c2f152170
danigoldblat <d0504109212@gmail.com>
2025-10-30 12:45:36 +0200

    finish

--- core/deck.py
+++ core/deck.py
@@ -15,7 +15,8 @@ def build_standard_deck() -> list[dict]:
     return in_place    
 
 # def shuffle_by_suit(deck: list[dict], swaps: int = 5000) -> list[dict]:
-   
-    
+#     swaps=5000
+Random_card=random.randrange(len(build_standard_deck()))
+
     
        
